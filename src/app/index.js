a7587154708358d7ea52606949a1fda427efd05d
Robin Hood <fordarnold@gmail.com>
2020-05-15 21:03:42 +0200

    feature(components): setup our first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,16 @@
-console.log('\n *** Hello from React Shopper *** \n\n');
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="card-panel teal">
+        <h1>Hello from React Shopper!!!</h1>
+      </div>
+    );
+  }
+}
+
+// NOTE: You can only return ONE single root (top-level) HTML element per Component.
+
+render(<App />, window.document.getElementById('app'));
