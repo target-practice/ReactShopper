72ad1a58b0b5ebc0e0bcd6722212658d14c31ea7
Robin Hood <fordarnold@gmail.com>
2020-05-15 21:21:32 +0200

    feature(components): nest multiple components

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+
 class App extends React.Component {
   render() {
     return (
-      <div className="card-panel teal">
-        <h1>Hello from React Shopper!!!</h1>
+      <div className="container">
+        <div className="row">
+          <Header />
+        </div>
+        <div className="row">
+          <h1>Hello from React Shopper!!!</h1>
+          <Home />
+        </div>
       </div>
     );
   }
